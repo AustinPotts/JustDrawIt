e59cef5774db68b647acb3af6a962b160ecb60ab
AustinPotts <pottsevolvements@gmail.com>
2019-09-15 00:44:33 -0400

    Created UIView Subclass

---
+++ JustDraw/DrawView.swift
@@ -0,0 +1,21 @@
+//
+//  DrawView.swift
+//  JustDraw
+//
+//  Created by Austin Potts on 9/15/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class DrawView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
