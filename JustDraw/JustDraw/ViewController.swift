adc11f275ce64d89b0b833a1ecdad22ba10fad23
AustinPotts <pottsevolvements@gmail.com>
2019-09-15 00:39:32 -0400

    First Commit

---
+++ JustDraw/JustDraw/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JustDraw
+//
+//  Created by Austin Potts on 9/15/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
